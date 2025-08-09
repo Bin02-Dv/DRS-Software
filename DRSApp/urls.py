f1db74f6c53b40de5f17bef090a251c5ecbc9e7c
Bin02-Dv <alaminbinahmad@gmail.com>
2025-08-09 08:26:16 -0700

    8/9/2025 - 8:26am

--- DRSApp/urls.py
+++ DRSApp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('about/', views.about, name='about'),
+    path('search/', views.search, name='search'),
+    path('dash/', views.dash, name='dash'),
 ]
--- DRSApp/views.py
+++ DRSApp/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def dash(request):
+    return render(request, 'dash/dashboard.html')
